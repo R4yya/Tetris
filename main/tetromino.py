034f139487534f6bc28bbab1df1cca1da775a3e4
R4yya <121268707+R4yya@users.noreply.github.com>
2023-09-15 21:42:40 +0300

    Create tetromino.py

---
+++ main/tetromino.py
@@ -0,0 +1,15 @@
+class Tetromino(object):
+    def __init__(self):
+        pass
+
+    def move(self):
+        pass
+
+    def rotate(self):
+        pass
+
+    def check_collision(self):
+        pass
+
+    def draw(self):
+        pass
