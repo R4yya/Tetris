a544dbf979e181b443c36c22d60c04bc557c1bf8
R4yya <121268707+R4yya@users.noreply.github.com>
2023-09-15 21:42:45 +0300

    Create main.py

---
+++ main/main.py
@@ -0,0 +1,13 @@
+import pygame
+from tetromino import Tetromino
+from field import Field
+from score import Score
+
+
+class Game(object):
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
