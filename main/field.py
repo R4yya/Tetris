bfb2e4734ebb65ae39ea1080012e88a4dc6f3e4c
R4yya <121268707+R4yya@users.noreply.github.com>
2023-09-16 16:25:41 +0300

    Update field.py

--- main/field.py
+++ main/field.py
@@ -1,9 +1,27 @@
+import pygame
+
+
 class Field(object):
-    def __init__(self):
-        pass
+    def __init__(self, width, height, screen_width, screen_height):
+        self.width = width
+        self.height = height
 
-    def draw(self):
-        pass
+        self.grid_color = (100, 100, 100)
 
-    def check_lines(self):
-        pass
+        self.grid_spacing_x = screen_width // width
+        self.grid_spacing_y = screen_height // height
+
+    def draw_grid(self, screen):
+        for x in range(0, self.width * self.grid_spacing_x, self.grid_spacing_x):
+            pygame.draw.line(screen, self.grid_color, (x, 0),
+                             (x, self.height * self.grid_spacing_y))
+
+        for y in range(0, self.height * self.grid_spacing_y, self.grid_spacing_y):
+            pygame.draw.line(screen, self.grid_color, (0, y),
+                             (self.width * self.grid_spacing_x, y))
+
+        pygame.draw.line(screen, self.grid_color, (0, self.height * self.grid_spacing_y),
+                         (self.width * self.grid_spacing_x, self.height * self.grid_spacing_y))
+
+        pygame.draw.line(screen, self.grid_color, (self.width * self.grid_spacing_x, 0),
+                         (self.width * self.grid_spacing_x, self.height * self.grid_spacing_y))
