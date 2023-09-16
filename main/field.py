9aae987ba75db919bb12550e7ef0ac5a439823a8
R4yya <121268707+R4yya@users.noreply.github.com>
2023-09-16 17:07:44 +0300

    Update field.py

--- main/field.py
+++ main/field.py
@@ -2,26 +2,26 @@ import pygame
 
 
 class Field(object):
-    def __init__(self, width, height, screen_width, screen_height):
+    def __init__(self, width, height, cell_size):
         self.width = width
         self.height = height
+        self.cell_size = cell_size
+
+        self.field = [[0 for _ in range(width)] for _ in range(height)]
 
         self.grid_color = (100, 100, 100)
 
-        self.grid_spacing_x = screen_width // width
-        self.grid_spacing_y = screen_height // height
-
     def draw_grid(self, screen):
-        for x in range(0, self.width * self.grid_spacing_x, self.grid_spacing_x):
+        for x in range(0, self.width * self.cell_size, self.cell_size):
             pygame.draw.line(screen, self.grid_color, (x, 0),
-                             (x, self.height * self.grid_spacing_y))
+                             (x, self.height * self.cell_size))
 
-        for y in range(0, self.height * self.grid_spacing_y, self.grid_spacing_y):
+        for y in range(0, self.height * self.cell_size, self.cell_size):
             pygame.draw.line(screen, self.grid_color, (0, y),
-                             (self.width * self.grid_spacing_x, y))
+                             (self.width * self.cell_size, y))
 
-        pygame.draw.line(screen, self.grid_color, (0, self.height * self.grid_spacing_y),
-                         (self.width * self.grid_spacing_x, self.height * self.grid_spacing_y))
+        pygame.draw.line(screen, self.grid_color, (0, self.height * self.cell_size),
+                         (self.width * self.cell_size, self.height * self.cell_size))
 
-        pygame.draw.line(screen, self.grid_color, (self.width * self.grid_spacing_x, 0),
-                         (self.width * self.grid_spacing_x, self.height * self.grid_spacing_y))
+        pygame.draw.line(screen, self.grid_color, (self.width * self.cell_size, 0),
+                         (self.width * self.cell_size, self.height * self.cell_size))
