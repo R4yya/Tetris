bf86a8346260b171ff808f7f50b0bf3ea46bb02e
R4yya <121268707+R4yya@users.noreply.github.com>
2023-09-22 01:41:01 +0300

    Update field.py

--- main/field.py
+++ main/field.py
@@ -11,6 +11,39 @@ class Field(object):
 
         self.grid_color = (100, 100, 100)
 
+    def check_lines(self):
+        lines_to_remove = []
+
+        for row in range(self.height):
+            if all(self.field[row]):
+                lines_to_remove.append(row)
+
+        for row in lines_to_remove:
+            del self.field[row]
+            self.field.insert(0, [0] * self.width)
+
+    def draw_fallen_tetromino(self, screen, tetromino):
+        current_shape = tetromino.current_shape
+        x, y = tetromino.x, tetromino.y
+
+        for row in range(len(current_shape)):
+            for col in range(len(current_shape[0])):
+                if current_shape[row][col] == 1:
+                    pygame.draw.rect(screen, tetromino.color, (x + col * self.cell_size,
+                                                               y + row * self.cell_size, self.cell_size, self.cell_size))
+
+    def update(self, screen, tetromino):
+        current_shape = tetromino.current_shape
+        x, y = tetromino.x, tetromino.y
+
+        for row in range(len(current_shape)):
+            for col in range(len(current_shape[0])):
+                if current_shape[row][col] == 1:
+                    self.field[y // self.cell_size +
+                               row][x // self.cell_size + col] = 1
+
+        self.draw_fallen_tetromino(screen, tetromino)
+
     def draw_grid(self, screen):
         for x in range(0, self.width * self.cell_size, self.cell_size):
             pygame.draw.line(screen, self.grid_color, (x, 0),
