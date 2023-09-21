07c49c95a5ffc1af79e6ada89b15933616482701
R4yya <121268707+R4yya@users.noreply.github.com>
2023-09-22 02:21:37 +0300

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,10 +1,39 @@
 from settings import *
 
 
-class Game(object):
+class Main(object):
     def __init__(self):
-        pass
+        pygame.init()
+
+        self.display_surface = pygame.display.set_mode(
+            (WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('Tetris')
+
+        self.clock = pygame.time.Clock()
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            match event.type:
+                case pygame.QUIT:
+                    self.running = False
+                case pygame.KEYDOWN:
+                    pass
+
+    def update(self):
+        pygame.display.update()
+
+    def run(self):
+        self.running = True
+
+        while self.running:
+            self.handle_events()
+
+            self.display_surface.fill(COLORS['GRAY'])
+
+            self.update()
+            self.clock.tick()
 
 
 if __name__ == '__main__':
-    pass
+    main = Main()
+    main.run()
