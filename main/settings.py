cb3b9093add7f4d84d55d0967b544b69ebdc7b13
R4yya <121268707+R4yya@users.noreply.github.com>
2023-09-22 02:21:34 +0300

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -1,42 +1,43 @@
 import pygame
 
-columns = 10
-rows = 20
-cell_size = 40
-game_width, game_height = columns * cell_size, rows * cell_size
+COLUMNS = 10
+ROWS = 20
+CELL_SIZE = 40
+GAME_WIDTH, GAME_HEIGHT = COLUMNS * CELL_SIZE, ROWS * CELL_SIZE
 
-sidebar_width = 200
-preview_height_fraction = 0.7
-score_preview_fraction = 1 - preview_height_fraction
+SIDEBAR_WIDTH = 200
+PREVIEW_HEIGHT_FRACTION = 0.7
+SCORE_HEIGHT_FRACTION = 1 - PREVIEW_HEIGHT_FRACTION
 
-padding = 20
-window_width = game_width + sidebar_width + padding * 3
-window_height = game_height + padding * 2
+PADDING = 20
+WINDOW_WIDTH = GAME_WIDTH + SIDEBAR_WIDTH + PADDING * 3
+WINDOW_HEIGHT = GAME_HEIGHT + PADDING * 2
 
-update_start_speed = 800
-move_wait_time = 200
-rotate_wait_time = 200
-block_offset = pygame.Vector2(columns // 2, -1)
+UPDATE_START_SPEED = 200
+MOVE_WAIT_TIME = 200
+ROTATE_WAIT_TIME = 200
+BLOCK_OFFSET = pygame.Vector2(COLUMNS // 2, -1)
 
-colors = {'yellow': '#f1e60d',
-          'red': '#e51b20',
-          'blue': '#204b9b',
-          'green': '#65b32e',
-          'purple': '#7b217f',
-          'cyan': '#6cc6d9',
-          'orange': '#f07',
-          'gray': '#1c1c1c',
-          'white': '#ffffff'}
 
-tetrominos = {'T': {'shape': [(0, 0), (-1, 0), (1, 0), (0, -1)], 'color': colors['purple']},
-              'O': {'shape': [(0, 0), (0, -1), (1, 0), (1, -1)], 'color': colors['yellow']},
-              'J': {'shape': [(0, 0), (0, -1), (0, 1), (-1, 1)], 'color': colors['blue']},
-              'L': {'shape': [(0, 0), (0, -1), (0, 1), (1, 1)], 'color': colors['orange']},
-              'I': {'shape': [(0, 0), (0, -1), (0, -2), (0, 1)], 'color': colors['cyan']},
-              'S': {'shape': [(0, 0), (-1, 0), (0, -1), (1, -1)], 'color': colors['green']},
-              'Z': {'shape': [(0, 0), (1, 0), (0, -1), (-1, -1)], 'color': colors['red']}}
+COLORS = {'YELLOW': '#f1e60d',
+          'RED': '#e51b20',
+          'BLUE': '#204b9b',
+          'GREEN': '#65b32e',
+          'PURPLE': '#7b217f',
+          'CYAN': '#6cc6d9',
+          'ORANGE': '#f07',
+          'GRAY': '#1c1c1c',
+          'WHITE': '#ffffff'}
 
-score_data = {1: 40, 2: 100, 3: 300, 4: 1200}
+TETROMINOS = {'T': {'shape': [(0, 0), (-1, 0), (1, 0), (0, -1)], 'color': COLORS['PURPLE']},
+              'O': {'shape': [(0, 0), (0, -1), (1, 0), (1, -1)], 'color': COLORS['YELLOW']},
+              'J': {'shape': [(0, 0), (0, -1), (0, 1), (-1, 1)], 'color': COLORS['BLUE']},
+              'L': {'shape': [(0, 0), (0, -1), (0, 1), (1, 1)], 'color': COLORS['ORANGE']},
+              'I': {'shape': [(0, 0), (0, -1), (0, -2), (0, 1)], 'color': COLORS['CYAN']},
+              'S': {'shape': [(0, 0), (-1, 0), (0, -1), (1, -1)], 'color': COLORS['GREEN']},
+              'Z': {'shape': [(0, 0), (1, 0), (0, -1), (-1, -1)], 'color': COLORS['RED']}}
+
+SCORE_DATA = {1: 40, 2: 100, 3: 300, 4: 1200}
 
 if __name__ == '__main__':
     pass
