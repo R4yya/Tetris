4d89706751fdacb02c92b015f3519899acbfd90f
R4yya <121268707+R4yya@users.noreply.github.com>
2023-09-22 05:08:19 +0300

    Update tetromino.py

--- main/tetromino.py
+++ main/tetromino.py
@@ -1,9 +1,15 @@
 from settings import *
 
+from block import Block
+
 
 class Tetromino(object):
-    def __init__(self):
-        pass
+    def __init__(self, shape, group):
+        self.block_positions = TETROMINOS[shape]['shape']
+        self.color = TETROMINOS[shape]['color']
+
+        self.blocks = [Block(group, position, self.color)
+                       for position in self.block_positions]
 
 
 if __name__ == '__main__':
