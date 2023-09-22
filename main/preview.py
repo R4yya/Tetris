c1e26e083e8ac9e7dffe71bc5ddee323a06057ee
R4yya <121268707+R4yya@users.noreply.github.com>
2023-09-22 23:14:29 +0300

    Update preview.py

--- main/preview.py
+++ main/preview.py
@@ -1,11 +1,16 @@
 from settings import *
 from base_model import BaseModel
+from os import path
 
 
 class Preview(BaseModel):
-    def __init__(self):
+    def __init__(self, next_shapes):
         self.surface = self.set_surface()
         self.display_surface = self.set_display_surface()
+
+        self.next_shapes = next_shapes
+        self.shapes_surfaces = {shape: pygame.image.load(path.join('..', 'graphics', f'{shape}.png')).convert_alpha() for shape in TETROMINOS.keys()}
+        self.rect = self.surface.get_rect(topright=(WINDOW_WIDTH - PADDING, PADDING))
 
     def set_surface(self):
         return pygame.Surface((SIDEBAR_WIDTH, GAME_HEIGHT * PREVIEW_HEIGHT_FRACTION))
@@ -14,8 +19,9 @@ class Preview(BaseModel):
         return pygame.display.get_surface()
 
     def run(self):
-        self.display_surface.blit(
-            self.surface, (PADDING * 2 + GAME_WIDTH, PADDING))
+        self.surface.fill(COLORS['GRAY'])
+        self.display_surface.blit(self.surface, self.rect)
+        pygame.draw.rect(self.display_surface, COLORS['WHITE'], self.rect, 2, 2)
 
 
 if __name__ == '__main__':
