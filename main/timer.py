4f94d03579f8aa51b2eb1b0de4d27532ac806b1a
R4yya <121268707+R4yya@users.noreply.github.com>
2023-09-22 05:46:44 +0300

    Update timer.py

--- main/timer.py
+++ main/timer.py
@@ -1,9 +1,34 @@
-from settings import *
+from pygame.time import get_ticks
 
 
 class Timer(object):
-    def __init__(self):
-        pass
+    def __init__(self, duration, repeated=False, function=None):
+        self.duration = duration
+        self.repeated = repeated
+        self.function = function
+
+        self.start_time = 0
+        self.active = False
+
+    def activate(self):
+        self.active = True
+        self.start_time = get_ticks()
+
+    def deactivate(self):
+        self.active = False
+        self.start_time = 0
+
+    def update(self):
+        current_time = get_ticks()
+
+        if current_time - self.start_time >= self.duration and self.active:
+            if self.function and self.start_time != 0:
+                self.function()
+
+            self.deactivate()
+
+            if self.repeated:
+                self.activate()
 
 
 if __name__ == '__main__':
