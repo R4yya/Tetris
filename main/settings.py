eea2ed19d3103910e7ec22627db6275f1643c023
R4yya <121268707+R4yya@users.noreply.github.com>
2023-09-22 05:47:02 +0300

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -13,32 +13,41 @@ PADDING = 20
 WINDOW_WIDTH = GAME_WIDTH + SIDEBAR_WIDTH + PADDING * 3
 WINDOW_HEIGHT = GAME_HEIGHT + PADDING * 2
 
-UPDATE_START_SPEED = 200
+UPDATE_START_SPEED = 800
 MOVE_WAIT_TIME = 200
 ROTATE_WAIT_TIME = 200
 BLOCK_OFFSET = pygame.Vector2(COLUMNS // 2, -1)
 
 
-COLORS = {'YELLOW': '#f1e60d',
-          'RED': '#e51b20',
-          'BLUE': '#204b9b',
-          'GREEN': '#65b32e',
-          'PURPLE': '#7b217f',
-          'CYAN': '#6cc6d9',
-          'ORANGE': '#f07e13',
-          'GRAY': '#1c1c1c',
-          'WHITE': '#ffffff',
-          'PURE_GREEN': '#00ff00'}
+COLORS = {
+    'YELLOW': '#f1e60d',
+    'RED': '#e51b20',
+    'BLUE': '#204b9b',
+    'GREEN': '#65b32e',
+    'PURPLE': '#7b217f',
+    'CYAN': '#6cc6d9',
+    'ORANGE': '#f07e13',
+    'GRAY': '#1c1c1c',
+    'WHITE': '#ffffff',
+    'PURE_GREEN': '#00ff00'
+}
 
-TETROMINOS = {'T': {'shape': [(0, 0), (-1, 0), (1, 0), (0, -1)], 'color': COLORS['PURPLE']},
-              'O': {'shape': [(0, 0), (0, -1), (1, 0), (1, -1)], 'color': COLORS['YELLOW']},
-              'J': {'shape': [(0, 0), (0, -1), (0, 1), (-1, 1)], 'color': COLORS['BLUE']},
-              'L': {'shape': [(0, 0), (0, -1), (0, 1), (1, 1)], 'color': COLORS['ORANGE']},
-              'I': {'shape': [(0, 0), (0, -1), (0, -2), (0, 1)], 'color': COLORS['CYAN']},
-              'S': {'shape': [(0, 0), (-1, 0), (0, -1), (1, -1)], 'color': COLORS['GREEN']},
-              'Z': {'shape': [(0, 0), (1, 0), (0, -1), (-1, -1)], 'color': COLORS['RED']}}
+TETROMINOS = {
+    'T': {'shape': [(0, 0), (-1, 0), (1, 0), (0, -1)], 'color': COLORS['PURPLE']},
+    'O': {'shape': [(0, 0), (0, -1), (1, 0), (1, -1)], 'color': COLORS['YELLOW']},
+    'J': {'shape': [(0, 0), (0, -1), (0, 1), (-1, 1)], 'color': COLORS['BLUE']},
+    'L': {'shape': [(0, 0), (0, -1), (0, 1), (1, 1)], 'color': COLORS['ORANGE']},
+    'I': {'shape': [(0, 0), (0, -1), (0, -2), (0, 1)], 'color': COLORS['CYAN']},
+    'S': {'shape': [(0, 0), (-1, 0), (0, -1), (1, -1)], 'color': COLORS['GREEN']},
+    'Z': {'shape': [(0, 0), (1, 0), (0, -1), (-1, -1)], 'color': COLORS['RED']}
+}
 
-SCORE_DATA = {1: 40, 2: 100, 3: 300, 4: 1200}
+SCORE_DATA = {
+    1: 40,
+    2: 100,
+    3: 300,
+    4: 1200
+}
 
 if __name__ == '__main__':
     pass
