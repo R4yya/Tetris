a18f3277379ac0caa911d5c6ea975460e65054a6
R4yya <121268707+R4yya@users.noreply.github.com>
2023-09-22 05:08:15 +0300

    Update block.py

--- main/block.py
+++ main/block.py
@@ -2,8 +2,15 @@ from settings import *
 
 
 class Block(pygame.sprite.Sprite):
-    def __init__(self):
-        pass
+    def __init__(self, group, position, color):
+        super().__init__(group)
+        self.image = pygame.Surface((CELL_SIZE, CELL_SIZE))
+        self.image.fill(color)
+
+        self.position = pygame.Vector2(position) + BLOCK_OFFSET
+        x = self.position.x * CELL_SIZE
+        y = self.position.y * CELL_SIZE
+        self.rect = self.image.get_rect(topleft=(x, y))
 
 
 if __name__ == '__main__':
