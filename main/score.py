e35f39a98bed4190a2486221effb3a3e15d15ffc
R4yya <121268707+R4yya@users.noreply.github.com>
2023-09-23 14:26:47 +0300

    Update score.py

--- main/score.py
+++ main/score.py
@@ -1,5 +1,8 @@
 from settings import *
+
 from base_model import BaseModel
+
+from os import path
 
 
 class Score(BaseModel):
@@ -9,14 +12,39 @@ class Score(BaseModel):
             WINDOW_WIDTH - PADDING, WINDOW_HEIGHT - PADDING))
         self.display_surface = self.set_display_surface()
 
+        self.border = self.surface.get_rect(topleft=(PADDING, PADDING))
+
+        self.font = pygame.font.Font(path.join('..', 'graphics', 'Russo_One.ttf'), 30)
+
+        self.increment_height = self.surface.get_height() / 3
+
+        self.lines = 0
+        self.score = 0
+        self.level = 1
+
+
     def set_surface(self):
         return pygame.Surface((SIDEBAR_WIDTH, GAME_HEIGHT * SCORE_HEIGHT_FRACTION - PADDING))
 
     def set_display_surface(self):
         return pygame.display.get_surface()
 
+    def display_text(self):
+        for i, text in enumerate([('Score', self.score), ('Level', self.level), ('Lines', self.lines)]):
+            x = self.surface.get_width() / 2
+            y = self.increment_height / 2 + i * self.increment_height
+            text_surface = self.font.render(f'{text[0]}: {text[1]}', True, COLORS['WHITE'])
+            text_rect = text_surface.get_rect(center=(x, y))
+            self.surface.blit(text_surface, text_rect)
+
     def run(self):
+        self.surface.fill(COLORS['GRAY'])
+
+        self.display_text()
+
         self.display_surface.blit(self.surface, self.rect)
+
+        pygame.draw.rect(self.display_surface, COLORS['WHITE'], self.rect, 3, 2)
 
 
 if __name__ == '__main__':
