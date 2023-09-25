e67c99b01e665eb15f94c1fef1360e0b4bce34a3
R4yya <121268707+R4yya@users.noreply.github.com>
2023-09-25 16:08:46 +0300

    Update main.py

--- main/main.py
+++ main/main.py
@@ -25,13 +25,15 @@ class Tetris(object):
 
         self.font = pygame.font.Font(path.join('..', 'graphics', 'Russo_One.ttf'), 30)
 
-        self.game = Game(self.get_next_shape, self.update_score)
+        self.game = Game(self.get_next_shape, self.update_score, self.font)
         self.score = Score(self.font)
         self.preview = Preview()
 
         self.background_music = pygame.mixer.Sound(path.join('..', 'sound', 'music.wav'))
         self.background_music.set_volume(0.05)
         self.background_music.play(-1)
+
+        self.game_over = False
 
     def handle_quit(self):
         for event in pygame.event.get():
@@ -54,14 +56,28 @@ class Tetris(object):
         while True:
             self.handle_quit()
 
-            self.display_surface.fill(COLORS['GRAY'])
+            if not self.game.game_over:
+                self.display_surface.fill(COLORS['GRAY'])
 
-            self.game.run()
-            self.score.run()
-            self.preview.run(self.next_shapes)
+                self.game.run()
+                self.score.run()
+                self.preview.run(self.next_shapes)
 
-            pygame.display.update()
-            self.clock.tick()
+                pygame.display.update()
+                self.clock.tick()
+            else:
+                self.game_over = False
+                self.score.reset_score()
+                self.game.reset_game()
+                self.game.show_game_over_screen()
+
+                waiting = True
+                while waiting:
+                    for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                            exit()
+                        if event.type == pygame.KEYDOWN:
+                            waiting = False
 
 
 if __name__ == '__main__':
